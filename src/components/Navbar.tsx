266497dd2bade2a5fbcb7099935642be45aab251
Hamdan-Khan <hamdankhan212@gmail.com>
2024-11-14 00:45:20 +0500

    navbar modified

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,19 @@
+"use client";
+
 import { FC } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  { id: 1, name: "Home", href: "/" },
+  { id: 2, name: "Blogs", href: "/blog" },
+];
 
 const Navbar: FC = () => {
+  const pathname = usePathname();
   return (
-    <div className="flex justify-between items-center py-3 pb-6">
+    <nav className="flex justify-between items-center py-3 pb-6">
       <Link href="/" className="flex items-center space-x-2">
         <div className="relative w-[30px] h-[30px]">
           <Image
@@ -16,7 +25,23 @@ const Navbar: FC = () => {
         </div>
         <span className="text-2xl font-bold none md:block">MatchMemo</span>
       </Link>
-    </div>
+      <ul className="flex gap-7 text-lg">
+        {links.map((link) => {
+          const { href, name, id } = link;
+          return (
+            <Link
+              key={id}
+              href={href}
+              className={`hover:text-white ${
+                pathname == href ? "font-semibold underline" : ""
+              }`}
+            >
+              {name}
+            </Link>
+          );
+        })}
+      </ul>
+    </nav>
   );
 };
 
