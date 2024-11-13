c5af02929e87cee6409205bf4cdd4a959451f592
Hamdan-Khan <hamdankhan212@gmail.com>
2024-11-13 23:35:18 +0500

    league matches api fixed

--- src/app/api/leagues/route.ts
+++ src/app/api/leagues/route.ts
@@ -2,17 +2,33 @@ import { options } from "@/lib/footballApi";
 
 export async function GET(req: Request) {
   try {
-    const { searchParams } = new URL(req.url);
-    const league = searchParams.get("league");
+    const url = new URL(req.url);
+    const league = url.searchParams.get("league");
+    const today = new Date();
+    const sevenDaysAgo = new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000);
+
+    const fromDate = `${sevenDaysAgo.getFullYear()}-${String(
+      sevenDaysAgo.getMonth() + 1
+    ).padStart(2, "0")}-${String(sevenDaysAgo.getDate()).padStart(2, "0")}`;
+    const sevenDaysFromNow = new Date(
+      today.getTime() + 7 * 24 * 60 * 60 * 1000
+    );
+
+    const toDate = `${sevenDaysFromNow.getFullYear()}-${String(
+      sevenDaysFromNow.getMonth() + 1
+    ).padStart(2, "0")}-${String(sevenDaysFromNow.getDate()).padStart(2, "0")}`;
 
     if (league) {
       const res = await fetch(
-        `http://api.football-data.org/v4/competitions/${league}/matches`,
+        `http://api.football-data.org/v4/competitions/${league}/matches?dateFrom=${fromDate}&dateTo=${toDate}`,
         options
       );
 
-      const result = await res.json();
-      return Response.json(result.competitions);
+      const response = await res.json();
+
+      const result = { status: 200, matches: response.matches };
+
+      return Response.json(result);
     } else {
       const res = await fetch(
         "http://api.football-data.org/v4/competitions/",
@@ -34,6 +50,6 @@ export async function GET(req: Request) {
       return Response.json(sortedCompetitions);
     }
   } catch (error) {
-    return Response.json({ error: "Server error", status: 500 });
+    return Response.json({ error: "Error in server", status: 500 });
   }
 }
