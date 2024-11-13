81dbbf13cac34a81d37889176863fc7cac8ded06
Hamdan-Khan <hamdankhan212@gmail.com>
2024-11-13 23:40:28 +0500

    categorize function added

--- src/utils/footballApi.ts
+++ src/utils/footballApi.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { options, tomorrow, yesterday } from "@/lib/footballApi";
 import { matchesType } from "@/types";
 
@@ -31,11 +32,41 @@ export const getTomorrowMatches = async () => {
   return res.json();
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const filterLeague = async (filterData: string, allMatches: any) => {
   const matches: matchesType[] = allMatches?.matches;
   const filteredLeagues = matches.filter(
     (item) => item.competition.name === filterData
   );
   return filteredLeagues;
+};
+
+export const categorizeMatches = (matches: any) => {
+  const today = new Date();
+  const tomorrowDate = new Date(today.getTime() + 24 * 60 * 60 * 1000);
+  const sevenDaysAgo = new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000);
+  const sevenDaysFromNow = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+
+  const todayMatches = [];
+  const earlierMatches = [];
+  const upcomingMatches = [];
+
+  for (const match of matches) {
+    const matchDate = new Date(match.utcDate);
+
+    if (matchDate >= today && matchDate < sevenDaysFromNow) {
+      if (matchDate >= today && matchDate < tomorrowDate) {
+        todayMatches.push(match);
+      } else {
+        upcomingMatches.push(match);
+      }
+    } else if (matchDate >= sevenDaysAgo && matchDate < today) {
+      earlierMatches.push(match);
+    }
+  }
+
+  return {
+    todayMatches,
+    earlierMatches,
+    upcomingMatches,
+  };
 };
