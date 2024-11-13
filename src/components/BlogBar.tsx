8d61897312ef6e289738215ffaea7fa838d20ff2
Hamdan-Khan <hamdankhan212@gmail.com>
2024-11-14 00:51:42 +0500

    blog bar modified

--- src/components/BlogBar.tsx
+++ src/components/BlogBar.tsx
@@ -1,6 +1,7 @@
 import BlogCard from "./BlogCard";
 import { baseURL } from "@/lib/footballApi";
 import { Blog } from "@prisma/client";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
@@ -12,14 +13,19 @@ export default async function BlogBar() {
   }
 
   const blogs: Blog[] = await res.json();
+  const sorted = blogs
+    .sort(
+      (a, b) => new Date(b.blogDate).getTime() - new Date(a.blogDate).getTime()
+    )
+    .slice(0, 5);
   return (
     <aside className="flex flex-col bg-[rgb(40,46,58)] px-2 md:px-4 py-2 rounded-md w-[300px] h-max pb-6">
       <div>
         <h1 className="font-bold text-xl mb-4 text-slate-400 text-center">
           Blogs
         </h1>
-        <ul className="flex flex-col gap-3">
-          {blogs.map((blog) => {
+        <div className="flex flex-col gap-3">
+          {sorted.map((blog) => {
             const { blogTitle, blogId } = blog;
             // const reducedContent =
             //   blogDescription.length <= 65
@@ -35,7 +41,14 @@ export default async function BlogBar() {
               />
             );
           })}
-        </ul>
+        </div>
+        {blogs.length > 5 ? (
+          <p className="mt-4 text-center">
+            <Link href="/blog" className="hover:underline">
+              View all...
+            </Link>
+          </p>
+        ) : null}
       </div>
     </aside>
   );
