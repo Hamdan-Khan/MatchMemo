e3db2fdfeb210f9f345b6956ef36f77d977ef3e8
Hamdan-Khan <hamdankhan212@gmail.com>
2024-11-13 23:59:43 +0500

    tf was that?

--- src/utils/footballApi.ts
+++ src/utils/footballApi.ts
@@ -1,36 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { options, tomorrow, yesterday } from "@/lib/footballApi";
 import { matchesType } from "@/types";
-
-export const getTodayMatches = async () => {
-  const res = await fetch("https://api.football-data.org/v4/matches", options);
-  if (!res.ok) {
-    return;
-  }
-  return res.json();
-};
-
-export const getYesterdayMatches = async () => {
-  const res = await fetch(
-    `https://api.football-data.org/v4/matches?date=${yesterday}`,
-    options
-  );
-  if (!res.ok) {
-    return;
-  }
-  return res.json();
-};
-
-export const getTomorrowMatches = async () => {
-  const res = await fetch(
-    `https://api.football-data.org/v4/matches?date=${tomorrow}`,
-    options
-  );
-  if (!res.ok) {
-    return;
-  }
-  return res.json();
-};
 
 export const filterLeague = async (filterData: string, allMatches: any) => {
   const matches: matchesType[] = allMatches?.matches;
