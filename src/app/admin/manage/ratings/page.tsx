5c33e3982ac25744446b2d96a33566b32f44fa09
hamdan-khan <hamdankhan212@gmail.com>
2024-11-16 18:54:59 +0500

    ratings comp modified

--- src/app/admin/manage/ratings/page.tsx
+++ src/app/admin/manage/ratings/page.tsx
@@ -1,3 +1,8 @@
 export default async function Ratings() {
-  return <div>Ratings</div>;
+  return (
+    <div className="min-h-screen bg-primary px-4">
+      <h1 className="font-semibold text-3xl text-white">Player Ratings</h1>
+      <h2 className="text-2xl text-gray-300">Coming soon!</h2>
+    </div>
+  );
 }
