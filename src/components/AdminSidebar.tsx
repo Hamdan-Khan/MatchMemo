35327eb86a95c92a26a4b8e8db1045bce852d6e0
hamdan-khan <hamdankhan212@gmail.com>
2024-11-16 18:53:29 +0500

    home link added

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation";
 import Button from "./Button";
 
 const adminUtils = [
+  { id: 0, name: "Home", href: "" },
   {
     id: 1,
     name: "Blogging",
@@ -20,7 +21,7 @@ const adminUtils = [
 export const AdminSidebar: React.FC = async () => {
   const pathname = usePathname();
   return (
-    <section className="px-2 md:px-4 py-4 bg-[rgb(40,46,58)] rounded-md min-w-[300px]">
+    <section className="px-2 border border-zinc-700 md:px-4 py-4 bg-outer rounded-md min-w-[300px] max-h-max">
       <div>
         <h1 className="font-bold text-xl text-center mb-4 text-[rgb(137,160,223)]">
           Manage
@@ -29,19 +30,14 @@ export const AdminSidebar: React.FC = async () => {
           {adminUtils.map((util) => (
             <Link href={`/admin/manage/${util.href}`} key={util?.id}>
               <div
-                className={`py-2 my-1 px-3 duration-500 rounded-md hover:bg-[rgb(47,54,68)] ${
-                  pathname == `/admin/manage/${util.href}`
-                    ? "bg-[rgb(45,53,68)]"
-                    : ""
+                className={`py-2 my-1 px-3 duration-500 rounded-md hover:bg-inner ${
+                  pathname == `/admin/manage/${util.href}` ? "bg-inner" : ""
                 }`}
               >
                 {util.name}
               </div>
             </Link>
           ))}
-          <Link href={"/api/auth/signout"}>
-            <Button color="red">Sign out</Button>
-          </Link>
         </ul>
       </div>
     </section>
