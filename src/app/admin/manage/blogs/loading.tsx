0de76534ad2d4f37b1a64ea88a430400cd0df3ed
hamdan-khan <hamdankhan212@gmail.com>
2024-11-20 21:05:36 +0500

    loading components improved

--- src/app/admin/manage/blogs/loading.tsx
+++ src/app/admin/manage/blogs/loading.tsx
@@ -1,3 +1,12 @@
 export default function Loading() {
-  return <p>Loading ...</p>;
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="text-center">
+        <div className="flex justify-center items-center mb-4">
+          <div className="w-12 h-12 border-4 border-t-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+        </div>
+        <p className="text-xl text-gray-600 font-semibold">Loading...</p>
+      </div>
+    </div>
+  );
 }
