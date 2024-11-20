df795add34fbd9cacb46e682ec2fed19432507bc
Hamdan-Khan <hamdankhan212@gmail.com>
2024-11-20 23:17:33 +0500

    loading fixed

--- src/app/team/[slug]/loading.tsx
+++ src/app/team/[slug]/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="flex justify-center items-center min-h-screen w-screen">
       <div className="text-center">
         <div className="flex justify-center items-center space-x-2 mb-4">
           <div className="w-4 h-4 bg-blue-500 rounded-full animate-bounce"></div>
